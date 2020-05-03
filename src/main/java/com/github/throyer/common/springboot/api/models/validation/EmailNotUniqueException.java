d4950b701b4b20875207d3372224f061fd1e7528
Renato Henrique <renato.henrique_98@hotmail.com>
2020-05-03 16:44:02 -0300

    Melhoria na validaçaõ de unicidade dos emails

---
+++ src/main/java/com/github/throyer/common/springboot/api/models/validation/EmailNotUniqueException.java
@@ -0,0 +1,35 @@
+package com.github.throyer.common.springboot.api.models.validation;
+
+import java.util.List;
+
+public class EmailNotUniqueException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    private List<SimpleError> errors;
+
+    public EmailNotUniqueException(List<SimpleError> errors) {
+        super();
+        this.errors = errors;
+    }
+
+    public EmailNotUniqueException(String message, List<SimpleError> errors) {
+        super(message);
+        this.errors = errors;
+    }
+
+    public EmailNotUniqueException(String message, Throwable cause, List<SimpleError> errors) {
+        super(message, cause);
+        this.errors = errors;
+    }
+
+    public EmailNotUniqueException(Throwable cause, List<SimpleError> errors) {
+        super(cause);
+        this.errors = errors;
+    }
+
+    public List<SimpleError> getErrors() {
+        return errors;
+    }
+
+}
