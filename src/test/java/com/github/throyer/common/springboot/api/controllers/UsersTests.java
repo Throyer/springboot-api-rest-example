3fe376b602f27fc36b422603cc6f6b3a71b1b7c8
Renato Henrique <renato.henrique_98@hotmail.com>
2021-08-08 03:04:07 -0300

    Tests

--- src/test/java/com/github/throyer/common/springboot/api/controllers/UsersTests.java
+++ src/test/java/com/github/throyer/common/springboot/api/controllers/UsersTests.java
@@ -11,7 +11,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
