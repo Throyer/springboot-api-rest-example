bcd001cab716a64399cab0391b6f0b5db4beb991
Renato Henrique <renato.henrique_98@hotmail.com>
2021-11-07 13:17:56 -0300

    Statics

--- src/main/java/com/github/throyer/common/springboot/configurations/SpringWebConfiguration.java
+++ src/main/java/com/github/throyer/common/springboot/configurations/SpringWebConfiguration.java
@@ -9,7 +9,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableWebMvc
 @Configuration
 public class SpringWebConfiguration implements WebMvcConfigurer {
 
@@ -24,17 +23,6 @@ public class SpringWebConfiguration implements WebMvcConfigurer {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-            .addResourceHandler("/**")
-                .addResourceLocations("classpath:/static/");
-        
-        registry
-            .addResourceHandler("/webjars/**")
-                .addResourceLocations("/webjars/");
     }
 
     @Bean
