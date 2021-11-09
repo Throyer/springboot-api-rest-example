01b7d0d1742b859f5ff090b8fe34b27ed686cbc1
Renato Henrique <renato.henrique_98@hotmail.com>
2021-11-09 17:10:33 -0300

    Inprove email template

--- src/main/java/com/github/throyer/common/springboot/domain/models/emails/RecoveryEmail.java
+++ src/main/java/com/github/throyer/common/springboot/domain/models/emails/RecoveryEmail.java
@@ -5,21 +5,11 @@ import com.github.throyer.common.springboot.domain.services.email.Email;
 import org.thymeleaf.context.Context;
 
 public class RecoveryEmail implements Email {
-    
-    private static final Integer FIRST = 0;
-    private static final Integer SECOND = 1;
-    private static final Integer THIRD = 2;
-    private static final Integer FOURTH = 3;
-
     private final String destination;
     private final String subject;
     private final String name;
+    private final String code;
     
-    private final String first;
-    private final String second;
-    private final String third;
-    private final String fourth;
-
     public RecoveryEmail(
         String destination,
         String subject,
@@ -29,13 +19,7 @@ public class RecoveryEmail implements Email {
         this.destination = destination;
         this.subject = subject;
         this.name = name;
-        
-        var code = recoveryCode.split("");
-        
-        this.first = code[FIRST];
-        this.second = code[SECOND];
-        this.third = code[THIRD];
-        this.fourth = code[FOURTH];
+        this.code = recoveryCode;
     }
 
     @Override
@@ -57,12 +41,8 @@ public class RecoveryEmail implements Email {
     public Context getContext() {
         var context = new Context();
         
-        context.setVariable("name", this.name);
-        
-        context.setVariable("first", this.first);
-        context.setVariable("second", this.second);
-        context.setVariable("third", this.third);
-        context.setVariable("fourth", this.fourth);
+        context.setVariable("name", this.name);        
+        context.setVariable("code", this.code);
         
         return context;
     }
