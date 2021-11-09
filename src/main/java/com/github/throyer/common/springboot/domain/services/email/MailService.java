70985bd25e8e6c72fde2f8a8f675b24060649632
Renato Henrique <renato.henrique_98@hotmail.com>
2021-11-09 14:39:55 -0300

    Email

--- src/main/java/com/github/throyer/common/springboot/domain/services/email/MailService.java
+++ src/main/java/com/github/throyer/common/springboot/domain/services/email/MailService.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -32,7 +33,8 @@ public class MailService {
             helper.setSubject(email.getSubject());
             helper.setText(engine.process(email.getTemplate(), email.getContext()), CONTENT_IS_HTML);
             sender.send(message);
-        } catch (MessagingException exception) {
+            LOGGER.info("Email enviado com sucesso para: {}", email.getDestination());
+        } catch (MessagingException | MailException exception) {
             LOGGER.error(ERROR_MESSAGE, exception);
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, ERROR_MESSAGE);
         }
