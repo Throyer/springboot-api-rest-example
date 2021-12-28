b72757890b103d35159789f6bea23925c0e4c0b1
Renato Henrique <renato.henrique_98@hotmail.com>
2021-12-28 17:51:31 -0300

    Improve recovery forms

--- src/main/java/com/github/throyer/common/springboot/domain/services/user/dto/Update.java
+++ src/main/java/com/github/throyer/common/springboot/domain/services/user/dto/Update.java
@@ -1,22 +1,17 @@
 package com.github.throyer.common.springboot.domain.services.user.dto;
 
-import static com.github.throyer.common.springboot.domain.services.user.dto.CreateUserApi.STRONG_PASSWORD;
-import static com.github.throyer.common.springboot.domain.services.user.dto.CreateUserApi.STRONG_PASSWORD_MESSAGE;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.BeanUtils;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 
 @Data
 @NoArgsConstructor
 public class Update {
-
-    private static final String CONFIRM_ERROR_MESSAGE = "Valor informado na confirmação de senha invalido.";
 
     @Email
     @NotNull
@@ -29,21 +24,14 @@ public class Update {
     private String fourth = "";
 
     @NotEmpty(message = "Por favor, forneça uma senha.")
-    @Pattern(regexp = STRONG_PASSWORD, message = STRONG_PASSWORD_MESSAGE)
+    @Size(min = 8, max = 255, message = "A senha deve conter no minimo {min} caracteres.")
     private String password;
-
-    @NotEmpty(message = "Por favor, confirme a senha.")
-    private String confirmPassword;
 
     public Update(Codes codes) {
         BeanUtils.copyProperties(codes, this);
     }
         
-    public void validate(BindingResult result) {
-        if (!getConfirmPassword().equals(getPassword())) {
-            result.addError(new ObjectError("confirmPassowrd", CONFIRM_ERROR_MESSAGE));
-        }
-    }
+    public void validate(BindingResult result) { }
 
     public String code() {
         return String.format("%s%s%s%s", first, second, third, fourth);
