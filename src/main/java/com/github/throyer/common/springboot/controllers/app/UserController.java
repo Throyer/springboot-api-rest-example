0f081d9905b15e67e277d30eabe8998488799b04
Renato Henrique <renato.henrique_98@hotmail.com>
2021-12-30 02:41:27 -0300

    Remove user feature on app

--- src/main/java/com/github/throyer/common/springboot/controllers/app/UserController.java
+++ src/main/java/com/github/throyer/common/springboot/controllers/app/UserController.java
@@ -2,16 +2,21 @@ package com.github.throyer.common.springboot.controllers.app;
 
 import com.github.throyer.common.springboot.domain.models.pagination.Page;
 import com.github.throyer.common.springboot.domain.models.pagination.Pagination;
+import com.github.throyer.common.springboot.domain.models.shared.Type;
 import com.github.throyer.common.springboot.domain.repositories.UserRepository;
-import com.github.throyer.common.springboot.domain.services.user.FindUserService;
+import com.github.throyer.common.springboot.domain.services.user.RemoveUserService;
 import com.github.throyer.common.springboot.domain.services.user.dto.SearchUser;
+import com.github.throyer.common.springboot.utils.Toasts;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @PreAuthorize("hasAnyAuthority('ADM')")
@@ -21,13 +26,25 @@ public class UserController {
     @Autowired
     private UserRepository repository;
     
+    @Autowired
+    private RemoveUserService removeService;
+    
     @GetMapping
-    public String idnex(Model model, Pagination pagination, Sort sort, SearchUser search) {
+    public String index(Model model, Pagination pagination, Sort sort, SearchUser search) {
         
         var page = Page.of(repository.findSimplifiedUsers(pagination.build()));
         
         model.addAttribute("page", page);
         
         return "app/users/index";
+    }
+    
+    @PostMapping("/delete/{id}")
+    public String delete(@PathVariable Long id, RedirectAttributes redirect) {
+        removeService.remove(id);
+        
+        Toasts.add(redirect, "Usuário deletado com sucesso.", Type.SUCCESS);
+        
+        return "redirect:/app/users";
     }
 }
--- src/main/resources/static/js/users-modal.js
+++ src/main/resources/static/js/users-modal.js
@@ -1,1 +1,7 @@
-console.log("gg")
+document.querySelector("#delete-confirm")
+    .addEventListener("show.bs.modal", ({ target: modal, relatedTarget: button }) => {
+        const { id, name } = button.dataset;
+
+        modal.querySelector("h5 > span").innerHTML = name;
+        modal.querySelector("form").action = `/app/users/delete/${id}`;
+    });
