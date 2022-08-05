ed3b357d90962ba78169c75041d5a2f0c3078c51
Renato Henrique <renato.henrique_98@hotmail.com>
2022-08-04 18:12:24 -0700

    add platform

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,16 +1,18 @@
+#!/bin/bash
+
 if [ -z "$1" ]; then
   echo "invalid argument"
-  return
+  exit
 fi
 
 if [ $1 = "up" ]; then
   docker-compose -p example-api-development -f ./docker/docker-compose.dev.yml --env-file ./docker/.env up -d --force-recreate
-  return
+  exit
 fi
 
 if [ $1 = "down" ]; then
   docker-compose -p example-api-development -f ./docker/docker-compose.dev.yml down
-  return
+  exit
 fi
 
 echo "invalid argument"
--- scripts/prod.sh
+++ scripts/prod.sh
@@ -1,16 +1,18 @@
+#!/bin/bash
+
 if [ -z "$1" ]; then
   echo "invalid argument"
-  return
+  exit
 fi
 
 if [ $1 = "up" ]; then
   docker-compose -p example-api -f ./docker/docker-compose.prod.yml --env-file ./docker/.env up -d --build
-  return
+  exit
 fi
 
 if [ $1 = "down" ]; then
   docker-compose -p example-api -f ./docker/docker-compose.prod.yml down
-  return
+  exit
 fi
 
 echo "invalid argument"
