f5b96ee476873173aea0a373e372be7f55256cdc
Renato Henrique <renato.henrique_98@hotmail.com>
2022-08-10 11:13:20 -0300

    feat: api home quotes

--- api/src/main/java/com/github/throyer/common/springboot/controllers/api/ApiController.java
+++ api/src/main/java/com/github/throyer/common/springboot/controllers/api/ApiController.java
@@ -1,11 +1,15 @@
 package com.github.throyer.common.springboot.controllers.api;
 
-import com.github.throyer.common.springboot.utils.Hello;
+import java.util.List;
+import java.util.Random;
 
-import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.github.throyer.common.springboot.utils.Hello;
+
+import io.swagger.v3.oas.annotations.Operation;
 
 @RestController
 @RequestMapping("/api")
@@ -14,6 +18,23 @@ public class ApiController {
     @GetMapping
     @Operation(hidden = true)
     public Hello index() {
-        return () -> "Is a live!";
+
+        var quotes = List.of(
+            "You talking to me? - Taxi Driver",
+            "I'm going to make him an offer he can't refuse. - The Godfather",
+            "May the Force be with you. - Star Wars",
+            "You're gonna need a bigger boat. - Jaws",
+            "Dadinho é o caralho! meu nome é Zé Pequeno, porra!",
+            "Say “hello” to my little friend! - Scarface",
+            "Bond. James Bond. - Dr. No",
+            "Hasta la vista, baby. - Terminator 2",
+            "I see dead people. - The Sixth Sense",
+            "Houston, we have a problem. - Apollo 13",
+            "Só sei que foi assim. - O Auto da Compadecida"
+        );
+
+        var quote = quotes.get(new Random().nextInt(quotes.size()));
+
+        return () -> quote;
     }
 }
