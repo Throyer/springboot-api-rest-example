879fdb232da20b6432544a5fd21b4f461d74fa46
Renato <renato.henrique_98@hotmail.com>
2023-09-03 02:27:40 -0300

    fix response message

--- server/src/main/java/com/github/throyer/example/api/domain/authentication/swagger/UsernameOrPasswordInvalidResponse.java
+++ server/src/main/java/com/github/throyer/example/api/domain/authentication/swagger/UsernameOrPasswordInvalidResponse.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
 
 public interface UsernameOrPasswordInvalidResponse {
-  @Schema(example = "Email was not confirmed.", requiredMode = REQUIRED)
+  @Schema(example = "Invalid password or username.", requiredMode = REQUIRED)
   String getMessage();
 
   @Schema(example = "403", requiredMode = REQUIRED)
